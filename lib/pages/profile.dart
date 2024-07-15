df8aa5f2f94b7a002723c42d5025be6b340bebab
FreanCatibog <freanrajascatibog@gmail.com>
2024-07-15 15:38:03 +0800

    By freanatics

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:gradient_elevated_button/gradient_elevated_button.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -13,10 +14,10 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.orange[600],
       appBar: AppBar(
         title: Text('User Profile'),
-        backgroundColor: Colors.orange[400],
+        backgroundColor: Colors.amber[700],
         centerTitle: true,
       ),
       body: Padding(
@@ -27,40 +28,67 @@ class _ProfileState extends State<Profile> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/human.png'),
-                    backgroundColor: Colors.orange[400],
-                    radius: 50.0,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 50.0, 0, 0),
+                  child: ExpandableCarousel(
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 5),
+                    ),
+                    items: [
+                      'assets/Carousel 1.jpg',
+                      'assets/Carousel 2.jpg',
+                      'assets/carousel 3.jpg',
+                      'assets/carousel 4.jpeg',
+                    ].map((String imagePath) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.pink
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Image.asset(
+                                  imagePath,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                          );
+                        },
+                      );
+                    }).toList(),
                   ),
                 ),
                 Divider(
                   height: 55.0,
-                  color: Colors.black,
+                  color: Colors.brown[800],
                   thickness: 5.0,
                 ),
                 Row(
                   children: [
                     Icon(
                       Icons.person_2,
-                      color:Colors.black87,
+                      color:Colors.grey[800],
                     ),
                     SizedBox(width: 3.0,),
                     Text(
                       'NAME',
                       style: TextStyle(
                         fontSize: 16.0,
-                        color: Colors.black87,
+                        color: Colors.white,
                         letterSpacing: 2.0,
                       ),
                     ),
                   ],
                 ),
                 Text(
-                  'Trixie Anne L. Catibog',
+                  'Trixie Anne Legaspi Catibog',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      color: Colors.black87,
+                      color: Colors.white,
                       fontSize: 20.0
                   ),
                 ),
@@ -69,14 +97,14 @@ class _ProfileState extends State<Profile> {
                   children: [
                     Icon(
                       Icons.calendar_month_sharp,
-                      color: Colors.black87,
+                      color: Colors.grey[800],
                     ),
                     SizedBox(width: 4.0,),
                     Text(
                       'YEAR',
                       style: TextStyle(
                         fontSize: 16.0,
-                        color: Colors.black87,
+                        color: Colors.white,
                         letterSpacing: 2.0,
                       ),
                     ),
@@ -86,7 +114,7 @@ class _ProfileState extends State<Profile> {
                   '$year Year',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Colors.black87,
+                    color: Colors.white,
                     fontSize: 20.0,
                   ),
                 ),
@@ -95,14 +123,14 @@ class _ProfileState extends State<Profile> {
                   children: [
                     Icon(
                       Icons.email_sharp,
-                      color: Colors.black87,
+                      color: Colors.grey[800],
                     ),
                     SizedBox(width: 4.0,),
                     Text(
                       'EMAIL',
                       style: TextStyle(
                         fontSize: 16.0,
-                        color: Colors.black87,
+                        color: Colors.white,
                         letterSpacing: 2.0,
                       ),
                     ),
@@ -112,7 +140,7 @@ class _ProfileState extends State<Profile> {
                   'trixieemacalalad@gmail.com',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Colors.black87,
+                    color: Colors.white,
                     fontSize: 20.0,
                   ),
                 ),
@@ -128,6 +156,22 @@ class _ProfileState extends State<Profile> {
                     });
                   },
                   child: Text('Add Year'),
+                ),
+                GradientElevatedButton(
+                  onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/login');
+                  },
+                  style: GradientElevatedButton.styleFrom(
+                    gradient: const LinearGradient(colors: [
+                      Color.fromARGB(255, 129, 133, 127),
+                      Color.fromARGB(225, 145, 175, 190),
+                    ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: const Text("LOG OUT YOUR ACCOUNT"),
+
                 ),
               ],
             ),
